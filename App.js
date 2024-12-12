2f3639e975fe9636c31be5732e53b584f5962c74
raofaeez <122422917+raofaeez@users.noreply.github.com>
2024-12-12 15:26:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,52 @@
+import render from "./render.js";
+import store from "./store.js"
+import { addTodo ,deleteTodo,toggleCompleted} from "./store.js";
+
+window.addEventListener("todoschange",()=>{
+    render();
+})
+ // todos  are array which are refrence type
+ //try to get store from localStorage
+ const storeFromLocalStorage = JSON.parse(localStorage.getItem("store"));
+ if (storeFromLocalStorage && storeFromLocalStorage.todos && storeFromLocalStorage.todos.length > 0) {
+     store.todos = storeFromLocalStorage.todos;
+ } else {
+     localStorage.setItem("store", JSON.stringify(store));
+     render();
+ }
+ 
+
+
+
+// form get
+
+const form =document.querySelector("#form");
+const todoTitleInput=document.querySelector(".todo-title-input")
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const todotitle=todoTitleInput.value;
+    const newTodo={id:crypto.randomUUID(),
+        title:todotitle,
+        completed:false}
+    addTodo(newTodo);
+
+});
+
+const todos =document.querySelector(".todos");
+
+todos.addEventListener("click",(e)=>{
+    const target=e.target
+    if(target.classList.contains("delete-todo-button")){
+      const id=  target.closest(".todo").dataset.id;
+      deleteTodo(id);
+    }
+})
+
+todos.addEventListener("change",(e)=>{
+    const target=e.target;
+    if(target.classList.contains("todo-checkbox")){
+        const id=target.closest(".todo").dataset.id;
+        const completed=target.checked;
+        toggleCompleted(id,completed);
+    }
+})
